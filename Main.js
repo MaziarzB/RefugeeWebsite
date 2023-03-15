0e495fb3ab7b6836bc94ca282e334d9c026a98c4
MaziarzB5 <104627594+MaziarzB@users.noreply.github.com>
2023-03-15 15:28:44 +0000

    Little bit of fixing up
    Changed text, and switched to UIcons

--- Main.js
+++ Main.js
@@ -1,10 +1,10 @@
-const blob = document.getElementById("blob");
+document.getElementById("cards").onmousemove = e => {
+  for(const card of document.getElementsByClassName("card")) {
+    const rect = card.getBoundingClientRect(),
+          x = e.clientX - rect.left,
+          y = e.clientY - rect.top;
 
-window.onpointermove = event => { 
-  const { clientX, clientY } = event;
-  
-  blob.animate({
-    left: `${clientX}px`,
-    top: `${clientY}px`
-  }, { duration: 3000, fill: "forwards" });
+    card.style.setProperty("--mouse-x", `${x}px`);
+    card.style.setProperty("--mouse-y", `${y}px`);
+  };
 }
